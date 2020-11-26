7e89e6e21397cb2dfaa1ae687fe2c6edc385b9dd
guopeng1129972 <381694179@qq.com>
2020-11-27 00:58:06 +0800

    5 6 7 使用状态机处理字符串
    5. 状态机 | 不使用状态机处理字符串（三）
    # 6. 状态机 | 使用状态机处理字符串（一）
    # 7. 状态机 | 使用状态机处理字符串（二）

--- Week 08/findString.js
+++ Week 08/findString.js
@@ -50,7 +50,7 @@
     console.log(`********demo find ab from ${string} *********`);
     let foundA = false;
     for (let c of string) {
-      if (c=="a")
+      if (c == "a")
         foundA = true;
       else if (foundA && c == "b")
         return true;
@@ -62,5 +62,51 @@
 
   console.log(match('I acbm groot'));
   console.log(match('I abm groot'));
+
+}
+
+/* 在一个字符串中找到字符串ABcdef */
+{
+  const findStr = (str) => {
+    console.log(`********my find abcdef from ${str} *********`);
+    if (typeof str !== 'string')
+      return 'str or word is not a string';
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === 'a') {
+        if (str[i + 1] === 'b')
+          if (str[i + 2] === 'c')
+            if (str[i + 3] === 'd')
+              if (str[i + 4] === 'e')
+                if (str[i + 5] === 'f')
+                  return true;
+      }
+    }
+    return false;
+  };
+  console.log(findStr('aaaaaaabcdef'));
+  console.log(findStr('aaaaaaabcdf'));
+
+}
+
+/* 在一个字符串中找到字符串ABcdef */
+/* 在一个字符串中找到字符串ABcdef  第二种办法*/
+{
+  const findStr = (str) => {
+    console.log(`********my2 find abcdef from ${str} *********`);
+    let findLenth = 6;
+    let findStr = 'abcdef';
+    if (typeof str !== 'string' && str.length <= findLenth)
+      return 'str or word is not a string or str is min';
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === 'a') {
+        if (str.substr(i, findLenth) === findStr)
+          return true;
+      }
+    }
+    return false;
+  };
+  console.log(findStr('abcdef'));
+  console.log(findStr('acadcabcdef'));
+  console.log(findStr('acadcabcde'));
 
 }
