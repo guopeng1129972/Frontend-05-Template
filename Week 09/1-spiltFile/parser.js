2380cbe04f9df8e29c483b34aca75943de658496
guopeng1129972 <381694179@qq.com>
2020-12-03 23:27:02 +0800

    3. HTML解析 | 解析标签

--- Week 09/1-spiltFile/parser.js
+++ Week 09/1-spiltFile/parser.js
@@ -1,7 +1,62 @@
 const EOF = Symbol('EOF');
 
 function data(c) {
+  if (c == '<')
+    return tagOpen;
+  else if (c == EOF)
+    return;
+  else
+    return data;
+}
 
+function tagOpen(c) {
+  if (c == '/')
+    return endTagOpen;
+  else if (c.match(/^[a-zA-Z]$/))
+    return tagName(c);
+  else
+    return;
+}
+
+function endTagOpen(c) {
+  if (c.match(/^[a-zA-Z]/))
+    return tagName(c);
+  else if (c == '>') {} else if (c == EOF)
+    return;
+  else {}
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/))
+    return beforeAttributeName;
+  else if (c == '/')
+    return selfCloseingStartTag;
+  else if (c.match(/^[a-zA-z]/))
+    return tagName;
+  else if (c == '>')
+    return data;
+  else
+    return tagName;
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/))
+    return beforeAttributeName;
+  else if (c == '>')
+    return data;
+  else if (c == '=')
+    return beforeAttributeName;
+  else
+    return beforeAttributeName;
+}
+
+function selfCloseingStartTag(c) {
+  if (c == '>') {
+    currentToken.isSelfClosing = true;
+    return data;
+  } else if (c == EOF)
+    return;
+  else {}
 }
 
 module.exports.parseHTML = function parseHTML(html) {
@@ -10,4 +65,5 @@ module.exports.parseHTML = function parseHTML(html) {
     state = state(c);
   }
   state = state(EOF);
+  console.log(html);
 }
--- Week 09/NOTE.md
+++ Week 09/NOTE.md
@@ -12,9 +12,29 @@ void async function () {
  let dom=parser.parseHTML(response.body);
 ```
 # 2. HTML解析 | 用FSM实现HTML的分析
-
+```js
+// match 的一个使用,字符串拆分
+  get response(){
+    // 'HTTP/1.1 200 OK '
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\S\s]+)/);
+    return {
+      statusCode:RegExp.$1,//200
+      statusText:RegExp.$2,//OK
+      headers:this.headers,
+      body:this.bodyParser.content.join('')
+    }
+  }
+```
 ## 第二步 用FSM实现HTML的分析
 
 - 我们用FSM来实现HTML的分析
 - 在HTML标准中，已经规定了HTML的状态
 - Toy-Borwser只挑选其中一部分状态，完成一个简易版本
+# 3. HTML解析 | 解析标签
+
+## 标签
+- 开始标签 
+- 结束标签 
+- 自封闭标签 
+- 暂时忽略属性
+##
