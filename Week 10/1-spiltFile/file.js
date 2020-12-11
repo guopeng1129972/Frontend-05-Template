138e98f3963039a95ca8c2b7d94a3a18a223eafd
guopeng1129972 <381694179@qq.com>
2020-12-11 21:26:32 +0800

    init 10

---
+++ Week 10/1-spiltFile/file.js
@@ -0,0 +1,131 @@
+[
+    {
+        "type": "rule",
+        "selectors": [
+            "#root"
+        ],
+        "declarations": [
+            {
+                "type": "declaration",
+                "property": "font-size",
+                "value": "13px",
+                "position": {
+                    "start": {
+                        "line": 3,
+                        "column": 3
+                    },
+                    "end": {
+                        "line": 3,
+                        "column": 18
+                    }
+                }
+            }
+        ],
+        "position": {
+            "start": {
+                "line": 2,
+                "column": 1
+            },
+            "end": {
+                "line": 4,
+                "column": 2
+            }
+        }
+    },
+    {
+        "type": "rule",
+        "selectors": [
+            "body div #myid"
+        ],
+        "declarations": [
+            {
+                "type": "declaration",
+                "property": "width",
+                "value": "100px",
+                "position": {
+                    "start": {
+                        "line": 6,
+                        "column": 1
+                    },
+                    "end": {
+                        "line": 6,
+                        "column": 13
+                    }
+                }
+            },
+            {
+                "type": "declaration",
+                "property": "background-color",
+                "value": "#ff5000",
+                "position": {
+                    "start": {
+                        "line": 7,
+                        "column": 1
+                    },
+                    "end": {
+                        "line": 7,
+                        "column": 26
+                    }
+                }
+            }
+        ],
+        "position": {
+            "start": {
+                "line": 5,
+                "column": 1
+            },
+            "end": {
+                "line": 8,
+                "column": 2
+            }
+        }
+    },
+    {
+        "type": "rule",
+        "selectors": [
+            "body div img"
+        ],
+        "declarations": [
+            {
+                "type": "declaration",
+                "property": "width",
+                "value": "30px",
+                "position": {
+                    "start": {
+                        "line": 10,
+                        "column": 1
+                    },
+                    "end": {
+                        "line": 10,
+                        "column": 12
+                    }
+                }
+            },
+            {
+                "type": "declaration",
+                "property": "background-color",
+                "value": "#ff1111",
+                "position": {
+                    "start": {
+                        "line": 11,
+                        "column": 1
+                    },
+                    "end": {
+                        "line": 11,
+                        "column": 26
+                    }
+                }
+            }
+        ],
+        "position": {
+            "start": {
+                "line": 9,
+                "column": 1
+            },
+            "end": {
+                "line": 12,
+                "column": 2
+            }
+        }
+    }
+]
