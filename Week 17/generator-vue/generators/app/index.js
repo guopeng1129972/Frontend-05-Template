da76bfd173f88a8d21bf84c273d9531cb5d3fadc
guopeng1129972 <381694179@qq.com>
2021-02-01 01:49:15 +0800

    3. 初始化与构建 | 初始化工具Yeoman（三）

--- Week 17/generator-vue/generators/app/index.js
+++ Week 17/generator-vue/generators/app/index.js
@@ -38,7 +38,26 @@ module.exports = class extends Generator {
     // Extend or create package.json file in destination path
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
     this.npmInstall(['vue'], { 'save-dev': false });
-    this.npmInstall(['webpack','vue-loader'], { 'save-dev': true });
+    this.npmInstall(['webpack','webpack-cli','vue-loader','vue-template-compiler','vue-style-loader','css-loader','copy-webpack-plugin'], { 'save-dev': true });
+    this.fs.copyTpl(
+      this.templatePath('HelloWorld.vue'),
+      this.destinationPath('src/HelloWorld.vue'),
+      {}
+    );
+    this.fs.copyTpl(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js'),
+      
+    );
+    this.fs.copyTpl(
+      this.templatePath('main.js'),
+      this.destinationPath('src/main.js'),
+      
+    );
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('src/index.html'),  
+      {title:answer.name} 
+    );
   }
-
 };
---
+++ Week 17/generator-vue/generators/app/templates/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack'); // 用于访问内置插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      use: 'vue-loader'
+    }, {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        'css-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyWebpackPlugin(
+      {
+        patterns: [
+          { from: 'src/*.html', to: '[name].[ext]' },
+        ],
+      }
+    ),
+  ],
+};
